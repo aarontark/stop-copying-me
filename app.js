bc02e07e35c41bdba12a2e54ba6efe96d8043612
aarontark <tarkington.aaron@gmail.com>
2025-09-28 21:11:18 -0500

    added botMessageRender function and applied styling to bot messages

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const messageWindow = document.querySelector('.message-window');
 const messageInput = document.querySelector('.message-input');
 
 // two functions: create and display user text, and create and display "bot" text
+// bot function: take message itself and length of it, and multiply that length by a certain number for setTimeout function
+
+const botMessageRender = (userText, userTextLength) => {
+    setTimeout(() => {
+        const botMessageHTML = `
+        <div class="bot-message-container message-container">
+            <div class="bot-message">${userText}</div>
+        </div>
+        `
+        messageWindow.innerHTML += botMessageHTML;
+    }, userTextLength * 50)
+}
 
 const userInputRender = (userText, eventKey) => {
     if (eventKey == 'Enter'){
@@ -10,8 +22,10 @@ const userInputRender = (userText, eventKey) => {
             <div class="user-message">${userText.value}</div>
         </div>
         `
+        const userMessage = userText.value;
         messageWindow.innerHTML += userMessageHTML;
         messageInput.value = '';
+        botMessageRender(userMessage, userMessage.length);
     }
 }
 
